6ec0671dad49dd40c413a58e46b0f693ab512fc9
peter <tmdals8798@gmail.com>
2024-11-16 22:54:53 +0900

    :sparkles: 설정 페이지 UI 구현 및 고객센터 이메일 기능 구현

--- presentation/src/main/java/com/whyranoid/presentation/screens/setting/SettingViewModel.kt
+++ presentation/src/main/java/com/whyranoid/presentation/screens/setting/SettingViewModel.kt
@@ -2,26 +2,36 @@ package com.whyranoid.presentation.screens.setting
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.whyranoid.domain.model.user.User
+import com.whyranoid.domain.repository.AccountRepository
 import com.whyranoid.domain.usecase.GetMyUidUseCase
-import com.whyranoid.domain.usecase.GetUserUseCase
+import com.whyranoid.presentation.screens.mypage.editprofile.UserInfoUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SettingViewModel(
+    private val accountRepository: AccountRepository,
     getMyUidUseCase: GetMyUidUseCase,
-    getUserUseCase: GetUserUseCase,
 ): ViewModel() {
-    private val _user = MutableStateFlow<User?>(null)
-    val user: StateFlow<User?> = _user.asStateFlow()
+    private val _userInfoUiState = MutableStateFlow<UserInfoUiState?>(null)
+    val userInfoUiState: StateFlow<UserInfoUiState?> = _userInfoUiState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            val uid = getMyUidUseCase().getOrNull()
-            val user = uid?.let { getUserUseCase(it).getOrNull() }
-            _user.value = user
+            val uid = getMyUidUseCase().getOrNull() ?: return@launch
+
+            accountRepository.getUserInfo(uid)
+                .onSuccess { userInfo ->
+                    _userInfoUiState.update {
+                        UserInfoUiState(
+                            userInfo.name,
+                            userInfo.nickname,
+                            userInfo.profileImg
+                        )
+                    }
+                }
         }
     }
 }
