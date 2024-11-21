652baf51b2a61eb2b7cd2fc31929dd0ed9330130
yonghanJu <ju990828@naver.com>
2024-11-21 23:12:22 +0900

    :sparkles: 회원가입 화면 하단 패딩 추가

--- presentation/src/main/java/com/whyranoid/presentation/screens/signin/SignInAgreeScreen.kt
+++ presentation/src/main/java/com/whyranoid/presentation/screens/signin/SignInAgreeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -46,6 +47,7 @@ fun SignInAgreeScreen(
         modifier = modifier
             .fillMaxSize()
             .background(Color.White)
+            .systemBarsPadding()
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
