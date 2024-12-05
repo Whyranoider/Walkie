95b2d6e76fdeb92233417724f3d6f66565b4b423
soopeach <hsjeon584@gmail.com>
2024-12-05 15:35:58 +0900

    :sparkles: 날짜 관련 유틸 추가

--- domain/src/main/java/com/whyranoid/domain/util/Extensions.kt
+++ domain/src/main/java/com/whyranoid/domain/util/Extensions.kt
@@ -1,6 +1,9 @@
 package com.whyranoid.domain.util
 
 import java.text.SimpleDateFormat
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 val String.Companion.EMPTY: String get() = ""
 
@@ -9,3 +12,25 @@ val String.Companion.BLANK: String get() = " "
 val String.Companion.DATE_FORMAT: String get() = "yyyy-MM-dd HH:mm:ss"
 
 val dateFormatter = SimpleDateFormat(String.DATE_FORMAT)
+
+fun getToday(): String {
+    val today = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern(String.DATE_FORMAT)
+    return today.format(formatter)
+}
+
+fun getDurationDifference(startDate: String, endDate: String): List<Long> {
+
+    val formatter = DateTimeFormatter.ofPattern(String.DATE_FORMAT)
+
+    val startDateTime = LocalDateTime.parse(startDate, formatter)
+    val endDateTime = LocalDateTime.parse(endDate, formatter)
+
+    val duration = Duration.between(endDateTime, startDateTime)
+    val days = duration.toDays()
+    val hours = duration.toHours() % 24
+    val minutes = duration.toMinutes() % 60
+
+    return listOf(days, hours, minutes)
+}
+
